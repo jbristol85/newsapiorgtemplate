71aa8152e6c4337a34a88a3435063badd859b60d
Josh Bristol <josh.bristol2@gmail.com>
2017-11-28 05:46:36 +0000

    2 apis working - pull headlines from source

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
 
 
 $(document).ready(function() {
+    
+
+
     $.ajax({
         method: "GET",
         url: "https://newsapi.org/v2/sources",
         data: { category: "business", country: "us", language: "en", apiKey: APIKEY },
         success: function(data) {
+            
             if (data.status === "ok") {
                 console.log(data);
                 for (var i = 0; i < data.sources.length; i++) {
@@ -14,14 +18,41 @@ $(document).ready(function() {
                     source.setAttribute("value", data.sources[i].id);
                     source.innerHTML = data.sources[i].name;
                     document.getElementById("selection").appendChild(source);
+                    // jsonData.push(JSON.parse(data));
                 }
+
             }
         }
 
     });
-    
+
+
     $('#source').submit(function(event) {
         event.preventDefault();
-        alert(document.getElementById("selection").value);
+        // alert(document.getElementById("selection").value);
+        var selectedNews = document.getElementById("selection").value;
+        $.ajax({
+            method: "GET",
+            url: "https://newsapi.org/v2/top-headlines",
+            data: { sources: selectedNews, category: "business", country: "us", language: "en", apiKey: APIKEY },
+            success: function(data) {
+                console.log(data);
+                if (data.status === "ok") {
+                    for (var i = 0; i < data.articles.length; i++) {
+                       
+                            console.log(data.articles[i].title);
+                            var headline = document.createElement("P");
+                            headline.innerHTML = data.articles[i].title;
+                            document.getElementById("headlines").appendChild(headline);
+                       
+                    }
+                }
+            }
+
+        });
+
     });
+
+
+   
 });
