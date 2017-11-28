a6f0e59d4c986868b41206ecb7aa8aa766f0e2f1
Josh Bristol <josh.bristol2@gmail.com>
2017-11-28 18:23:48 +0000

    titles loading with images

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 
 $(document).ready(function() {
-    
+
 
 
     $.ajax({
@@ -10,7 +10,7 @@ $(document).ready(function() {
         url: "https://newsapi.org/v2/sources",
         data: { category: "business", country: "us", language: "en", apiKey: APIKEY },
         success: function(data) {
-            
+
             if (data.status === "ok") {
                 console.log(data);
                 for (var i = 0; i < data.sources.length; i++) {
@@ -39,12 +39,19 @@ $(document).ready(function() {
                 console.log(data);
                 if (data.status === "ok") {
                     for (var i = 0; i < data.articles.length; i++) {
-                       
-                            console.log(data.articles[i].title);
-                            var headline = document.createElement("P");
-                            headline.innerHTML = data.articles[i].title;
-                            document.getElementById("headlines").appendChild(headline);
-                       
+
+                        // console.log(data.articles[i].title);
+                        // var headline = document.createElement("P");
+                        // headline.innerHTML = data.articles[i].title;
+                        // document.getElementById("headlines").appendChild(headline);
+                        var thumbnails = document.createElement("DIV");
+                        thumbnails.setAttribute("class", "thumbnail");
+                        thumbnails.setAttribute("class", "col-xs-4");
+                        thumbnails.setAttribute("style", "width: 30%");
+                        thumbnails.innerHTML = "<div class='thumbnail'><img class='img-responsive' src=" + data.articles[i].urlToImage + "></img><div class='caption'><h3><a href=" + data.articles[i].url + ">" + data.articles[i].title + "</a></h3></div></div>";
+
+                        document.getElementById("insertThumbs").appendChild(thumbnails);
+
                     }
                 }
             }
@@ -54,5 +61,5 @@ $(document).ready(function() {
     });
 
 
-   
+
 });
