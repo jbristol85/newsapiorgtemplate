a16910735f1bbba4f4c35454ebaba277e9d915d3
Josh Bristol <josh.bristol2@gmail.com>
2018-01-12 01:56:04 +0000

    API changed, fixed code

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
         $.ajax({
             method: "GET",
             url: "https://newsapi.org/v2/top-headlines",
-            data: { sources: selectedNews, category: "business", country: "us", language: "en", apiKey: APIKEY },
+            data: { sources: selectedNews,  language: "en", apiKey: APIKEY },
             success: function(data) {
                 console.log(data);
                 if (data.status === "ok") {
@@ -47,7 +47,7 @@ $(document).ready(function() {
                         // headline.innerHTML = data.articles[i].title;
                         // document.getElementById("headlines").appendChild(headline);
                         var thumbnails = document.createElement("DIV");
-                        thumbnails.setAttribute("class", "thumbnail");
+                        // thumbnails.setAttribute("class", "thumbnail");
                         thumbnails.setAttribute("class", "col-xs-4");
                         thumbnails.setAttribute("style", "width: 30%");
                         thumbnails.innerHTML = "<div class='thumbnail'><img class='img-responsive' src=" + data.articles[i].urlToImage + "></img><div class='caption'><h3><a href=" + data.articles[i].url + ">" + data.articles[i].title + "</a></h3><p>"+data.articles[i].description+"</p></div></div>";
